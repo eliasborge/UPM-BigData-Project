a6b9cb10173faef1ea9955f4139da0dbaf9a6949
Konrad Owsikowski <konrad.owsikowski@gmail.com>
2023-12-14 23:35:07 +0100

    Changed constant var name into upper case

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ from pyspark.sql import SparkSession
 
 from data_processor import DataReaderHDFS, DataReaderLocalFS, DataReader
 
-data_source = "LOCAL"
+DATA_SOURCE = "LOCAL"
 HDFS_SERVER_ADDRESS = "hdfs://localhost:9000"
 HDFS_DATA_DIRECTORY_PATH = "/assignment_data"
 LOCAL_DATA_DIRECTORY_PATH = "/home/konrad/Desktop/UPM/big_data/assignment_data"
 
 
 def get_data_reader(spark_session: SparkSession) -> DataReader:
-	if data_source == "HDFS":
+	if DATA_SOURCE == "HDFS":
 		data_reader = DataReaderHDFS(
 			spark_session=spark_session,
 			server_address=HDFS_SERVER_ADDRESS,
 			directory_path=HDFS_DATA_DIRECTORY_PATH
 		)
-	elif data_source == "LOCAL":
+	elif DATA_SOURCE == "LOCAL":
 		data_reader = DataReaderLocalFS(
 			spark_session=spark_session,
 			directory_path=LOCAL_DATA_DIRECTORY_PATH
 		)
 	else:
-		raise ValueError(f"Unknown data source: {data_source}")
+		raise ValueError(f"Unknown data source: {DATA_SOURCE}")
 	return data_reader
 
 
